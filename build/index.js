b2230a533d16cfa8d8f2a05416bf86c2890011f1
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-11-25 15:40:09 +0200

    Initial app configurations

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(123);
+var log = function () {
+    console.log('hi');
+};
+log();
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log(123);
+
+const log = () => {
+  console.log('hi');
+};
+log();
